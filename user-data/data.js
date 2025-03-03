569a572f8877f29309bab96d3f7c09a92863a426
Rambabu Chinta <ramvarmach@outlook.com>
2025-03-03 23:16:25 +0530

    Add files via upload

---
+++ user-data/data.js
@@ -0,0 +1,90 @@
+export const bio = [
+  "Hello, I'm Rambabu Chinta!",
+  "<strong>Senior Software Engineer</strong> with 4+ years of experience in the full Software Development Life Cycle (SDLC). I specialize in designing, developing, and delivering high-quality software solutions that drive business success. With a strong foundation in problem-solving and innovation, I excel in gathering requirements, analyzing complex problems, and creating efficient and scalable software systems. I'm passionate about adopting new technologies, improving processes, and mentoring teams to ensure continuous growth and excellence in development.",
+];
+
+export const skills = [
+  "JavaScript, AngularJS, HTML5, CSS, Bootstrap,Jquery",
+  "ASP.NET MVC, .NET Core, C#,.NET Core/Web API",
+  "SQL Server, Oracle, My Sql",
+];
+
+export const experience = [
+  {
+    title: "National Informatics Center (NIC)",
+    duration: "April 2024 - Present",
+    subtitle: "Senior Software Engineer",
+    details: [
+      "The SBTET Telangana website is a comprehensive digital platform designed to streamline and regulate technical education in Telangana.",
+      "Managed by the State Board of Technical Education and Training (SBTET), the site provides centralized access to essential resources,including course information, exam notifications, institutional details, and student services",
+      " Built with NET MVC, AngularJS, and SQL Server technologies, the platform offers a seamless",
+      "Interactive experience for students, institutions, and stakeholders, enhancing accessibility to key educational tools and information.",
+    ],
+    tags: ["JavaScript","Bootstrap", "AngularJs","ASP.NET MVC","SQL Server"],
+    icon: "truck ",
+  },
+  {
+    title: "VolkSoft Consulting Pvt. Ltd",
+    duration: "Feb 2022 - Mar 2024",
+    subtitle: "Software Engineer",
+    details: [
+      "Volksoft Consulting Pvt. Ltd. is a leading provider of innovative technology solutions, specializing in software development, IT consulting, and digital transformation services.",
+      "With a strong focus on delivering customized solutions, Volksoft helps businesses across various industries optimize their operations, enhance efficiency, and drive growth.",
+      "Their expertise spans a wide range of services, including system integration, cloud computing, enterprise applications, and data analytics. Committed to excellence, Volksoft Consulting Pvt. Ltd. is dedicated to empowering organizations with cutting-edge technology to stay ahead in today’s competitive market.",
+    ],
+    tags: [
+      "JavaScript",
+      "Bootstrap",
+      "ASP.NET MVC",
+      "SQL Server",
+    ],
+    icon: "heartbeat",
+  },
+  {
+    title: "Mavericks Business And Market Research Pvt. Ltd",
+    duration: "Jan 2021 - Jan 2022",
+    subtitle: "Software Engineer",
+    details: [
+      "Mavericks Business and Market Research Pvt. Ltd. is a dynamic firm based in Secunderabad, specializing in providing strategic business insights and market research services.",
+      " With a focus on data-driven solutions, Mavericks helps businesses make informed decisions, optimize performance, and drive growth in competitive markets.",
+    ],
+    tags: [ "JavaScript",
+      "Bootstrap",
+      "ASP.NET MVC",
+      "SQL Server",],
+    icon: "qrcode",
+  },
+];
+
+export const education = [
+  {
+    title: "B.Tech. in Electrical and Electronics Engineering",
+    duration: "2016 - 2020",
+    subtitle: "BVC Institute of Technology and Sciences, East Godavari, Andhra Pradesh",
+    details: [],
+    tags: [
+      
+    ],
+    icon: "graduation-cap",
+  }
+];
+
+export const footer = [
+  {
+    label: "Links",
+    data: [
+      {
+        text: "Buy me a coffee",
+        link: "https://www.buymeacoffee.com/r194dME8y",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/Rambabu-Chinta/Rambabu-Chinta.github.io",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: ["Made with &hearts; by Rambabu Chinta"],
+  },
+];
